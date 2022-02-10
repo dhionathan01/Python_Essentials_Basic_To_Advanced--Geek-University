fcd0ff1d0001e3af2cbb921a2bf0035b00d6d2ff
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-02-10 17:50:33 -0300

    Seção 13 - Sistema de arquivos : Manipulação - Refactor

--- Seção 13 - Leitura e Escrita em Arquivos/sistema_de_arquivos_manipulacao.py
+++ Seção 13 - Leitura e Escrita em Arquivos/sistema_de_arquivos_manipulacao.py
@@ -2,9 +2,9 @@
 Sistema de arquivos - Manipulação
 """
 
-
-'''
 import os
+'''
+
 # Descobrindo se um arquivo existe
 print(os.path.exists('arquivo.txt'))    # False
 print(os.path.exists('frutas.txt'))    # True
@@ -40,3 +40,43 @@ da ausência de identação
 """
 # Forma correta de criar arquivo - não disponível pra windows
 # os.mknod('arquivo_test4.txt')
+
+# Criando diretórios Únicos (Um a Um)
+'''
+os.mkdir('templates')  # Relativo
+
+os.mkdir('C:/Users/dhion/Documents/Git/Udemy--Curso--Python/Seção 13 - Leitura e Escrita em Arquivos')  # Absoluto
+'''
+# OBS: A função mkdir cria um diretório se este não existir, caso exista, teremos FileExistsError
+# OBS: Caso não tenhamos permissão para criar o diretório termos o erro PermissionError
+
+# Erro pois eles ta tentando criar o o diretório testando_novo_dir em novodir, sendo que o msm não existe
+# os.mkdir('templates/novodir/testando_novo_dir')
+
+# A alternativa que temos é o makedirs(Faça diretórios):
+
+# Criando multi-diretórios (árvore de diretórios)
+
+# os.makedirs('templates/novodir/testando_novo_dir')
+
+# OBS: Caso exista, teremos FileExistsError
+
+# os.makedirs('templates2/novo2/outro2', exist_ok=True)
+
+# OBS: exist_ok=True Faz com que caso o diretório exista apenas ignore e não exiba erros
+
+# Renomear arquivos e diretórios
+# Diretórios
+
+# os.rename('templates2', 'Renomeando_Templates2')
+
+# OBS: Se o diretório não exisitir teremos um FileNotFoundError
+# OBS: Se o diretório que queremos renomear não estiver vazio, teremos um OSError
+
+# Renomeando arquivos:
+# Renomeando arquivo : teste12314.txt, para rename_test.txt
+
+# os.rename('templates/novodir/testando_novo_dir/teste12314.txt', 'templates/novodir/testando_novo_dir/rename_test.txt')
+
+# Renomeando arquivo frutas.txt para cesta_frutas.txt
+# os.rename('frutas.txt', 'cesta_frutas.txt')

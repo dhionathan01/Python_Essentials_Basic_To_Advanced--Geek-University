48c3971f1737170267bd5e8ac3098f015801498c
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-02-16 15:53:08 -0300

    Seção 13 - exercicio_04.py - Adicionado uma excpet na contagem

--- Seção 13 - Leitura e Escrita em Arquivos/Exercícios/exercicio_04/exercicio_04.py
+++ Seção 13 - Leitura e Escrita em Arquivos/Exercícios/exercicio_04/exercicio_04.py
@@ -20,24 +20,30 @@ def abrindo_arquivo(nome):
 
 
 def contar_vogais(arquivo_recebido):
-    lista_de_vogais = ['a', 'e', 'i', 'o', 'u']
-    contador_de_vogais = 0
-    for vogal in lista_de_vogais:
-        contador_de_vogais += arquivo_recebido.count(vogal.lower())  # verificando vogais que estão em caixa baixa
-        contador_de_vogais += arquivo_recebido.count(vogal.upper())  # verificando vogais que estão em caixa alta
-    return contador_de_vogais
+    try:
+        lista_de_vogais = ['a', 'e', 'i', 'o', 'u']
+        contador_de_vogais = 0
+        for vogal in lista_de_vogais:
+            contador_de_vogais += arquivo_recebido.count(vogal.lower())  # verificando vogais que estão em caixa baixa
+            contador_de_vogais += arquivo_recebido.count(vogal.upper())  # verificando vogais que estão em caixa alta
+        return contador_de_vogais
+    except AttributeError:
+        print('Não foi possivel efetuar a contagem')
 
 
 def contar_consoantes(arquivo_recebido):
-    lista_de_consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
-                           'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-    contador_de_consoantes = 0
-    for consoante in lista_de_consoantes:
-        # verificando consoantes que estão em caixa baixa
-        contador_de_consoantes += arquivo_recebido.count(consoante.lower())
-        # verificando consoantes que estão em caixa alta
-        contador_de_consoantes += arquivo_recebido.count(consoante.upper())
-    return contador_de_consoantes
+    try:
+        lista_de_consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+                               'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+        contador_de_consoantes = 0
+        for consoante in lista_de_consoantes:
+            # verificando consoantes que estão em caixa baixa
+            contador_de_consoantes += arquivo_recebido.count(consoante.lower())
+            # verificando consoantes que estão em caixa alta
+            contador_de_consoantes += arquivo_recebido.count(consoante.upper())
+        return contador_de_consoantes
+    except AttributeError:
+        print('Não foi possivel efetuar a contagem')
 
 
 if __name__ == '__main__':

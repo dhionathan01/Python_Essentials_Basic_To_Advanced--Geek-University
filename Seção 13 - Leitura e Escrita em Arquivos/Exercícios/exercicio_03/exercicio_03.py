58bc26dedc497e8259cdb1287b11ee3ab8001b18
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-02-16 15:17:27 -0300

    Seção 13 - exercicio_03.py - Refatorado

--- Seção 13 - Leitura e Escrita em Arquivos/Exercícios/exercicio_03/exercicio_03.py
+++ Seção 13 - Leitura e Escrita em Arquivos/Exercícios/exercicio_03/exercicio_03.py
@@ -1,24 +1,36 @@
 """
 Faça um programa que recaba do usuário um arquivo texto e mostre na tela quantas letras são vogais.
 """
-try:
+
+
+def abrindo_arquivo(nome):
+    try:
+        with open(nome, 'r', encoding='utf-8') as arquivo:
+            arquivo = arquivo.read()
+            return arquivo
+
+    except FileExistsError:
+        print('\n Arquivo não existe')
+    except FileNotFoundError:
+        print('\n Arquivo não foi encontrado')
+    except OSError:
+        print(' \nNomes de arquivos não podem conter caracteres especiais')
+
+
+def contar_vogais(arquivo_recebido):
+    lista_de_vogais = ['a', 'e', 'i', 'o', 'u']
+    contador_de_vogais = 0
+    for vogal in lista_de_vogais:
+        contador_de_vogais += arquivo_recebido.count(vogal.lower())  # verificando vogais que estão em caixa baixa
+        contador_de_vogais += arquivo_recebido.count(vogal.upper())  # verificando vogais que estão em caixa alta
+    print(contador_de_vogais)
+
+
+if __name__ == '__main__':
     nome_arquivo = (input('Insira o nome / caminho do arquivo: '))
     if '.txt' not in nome_arquivo:
         nome_arquivo += '.txt'
 
-    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
-        contador_de_vogais = 0
-        arquivo = arquivo.read()
-        lista_de_vogais = ['a', 'e', 'i', 'o', 'u']
-        for vogal in lista_de_vogais:
-            contador_de_vogais += arquivo.count(vogal.lower())  # verificando vogais que estão em caixa baixa
-            contador_de_vogais += arquivo.count(vogal.upper())  # verificando vogais que estão em caixa alta
-        print(contador_de_vogais)
-
-except FileExistsError:
-    print('\n Arquivo não existe')
-except FileNotFoundError:
-    print('\n Arquivo não foi encontrado')
-except OSError:
-    print(' \nNomes de arquivos não podem conter caracteres especiais')
+    textos_recebidos = abrindo_arquivo(nome_arquivo)
+    contar_vogais(textos_recebidos)
 
